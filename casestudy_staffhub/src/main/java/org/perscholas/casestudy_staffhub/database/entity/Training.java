0a776fb2af4240c5a867767de4e92f53c166a258
getuengda <batworg12@gmail.com>
2023-12-21 15:55:24 -0500

    added User, Department, Training and UserTraining entities

---
+++ casestudy_staffhub/src/main/java/org/perscholas/casestudy_staffhub/database/entity/Training.java
@@ -0,0 +1,38 @@
+package org.perscholas.casestudy_staffhub.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name="trainings")
+public class Training {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String trainingName;
+
+    @Column(name="date_posted")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePosted;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "prerequisite")
+    private String prerequisite;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "user_id")
+    private Integer userId;
+}
---
+++ casestudy_staffhub/src/main/java/org/perscholas/casestudy_staffhub/database/entity/UserTraining.java
@@ -0,0 +1,38 @@
+package org.perscholas.casestudy_staffhub.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name="employee_training")
+public class UserTraining {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id")
+    private Training training;
+
+    @Column(name = "enrollment_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date enrollmentDate;
+
+    @Column(name = "completion_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date completionDate;
+
+    @Column(name = "status")
+    private String status;
+}
