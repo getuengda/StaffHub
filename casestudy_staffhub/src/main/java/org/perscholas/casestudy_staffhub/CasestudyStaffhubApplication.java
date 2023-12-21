15ccb8db05ad0da5220c471b43991472a773beb4
getuengda <batworg12@gmail.com>
2023-12-21 15:10:06 -0500

    added maven dependencies using spring io

---
+++ casestudy_staffhub/src/main/java/org/perscholas/casestudy_staffhub/CasestudyStaffhubApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.casestudy_staffhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyStaffhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyStaffhubApplication.class, args);
+	}
+
+}
---
+++ casestudy_staffhub/src/test/java/org/perscholas/casestudy_staffhub/CasestudyStaffhubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.casestudy_staffhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasestudyStaffhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
